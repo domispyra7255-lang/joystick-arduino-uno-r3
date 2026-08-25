02d36215354405887a25b3302ad02bed9b4a0736
p9olomer <domispyra7255@gmail.com>
2026-08-25 21:38:46 +0200

    Zmień przycisk na SPACJĘ zamiast kliknięcia myszki

--- joystick_position.ino
+++ joystick_position.ino
@@ -1,16 +1,19 @@
-// Arduino UNO R3 - Odczytywanie pozycji modułu Joystick (tylko przy zmianie)
-// LEWO I PRAWO ZAMIENIONE (OBRÓT NA OSI X)
+// Arduino UNO R3 - Joystick jako myszka USB + spacja na klawiaturze
+// LEWO/PRAWO OK, GÓRA/DÓŁ ZAMIENIONE
 // Pinowanie:
 // VRx -> A0 (analogowy)
 // VRy -> A1 (analogowy)
-// SW  -> Pin 2 (cyfrowy)
+// SW  -> Pin 2 (cyfrowy - wciśnięcie = SPACJA)
 // +5V -> 5V
 // GND -> GND
 
+#include <Mouse.h>
+#include <Keyboard.h>
+
 // Definiowanie pinów
-const int joystick_X = A0;  // Oś X
-const int joystick_Y = A1;  // Oś Y
-const int joystick_SW = 2;  // Przycisk (switch)
+const int joystick_X = A0;  // Oś X (lewo/prawo)
+const int joystick_Y = A1;  // Oś Y (góra/dół - ZAMIENIONE)
+const int joystick_SW = 2;  // Przycisk (switch) - SPACJA
 
 // Zmienne do przechowywania wartości
 int x_value = 0;
@@ -18,19 +21,26 @@ int y_value = 0;
 int button_value = 0;
 
 // Zmienne do przechowywania poprzednich wartości
-int prev_x_value = -1;
-int prev_y_value = -1;
+int prev_x_value = 512;
+int prev_y_value = 512;
 int prev_button_value = 1;
 
-// Próg czułości (zmiana musi być większa niż ten próg, aby się wyświetlić)
+// Próg czułości
 const int sensitivity_threshold = 30;
 
+// Prędkość ruchu myszki
+const int mouse_speed = 5;
+
 void setup() {
-  Serial.begin(9600);  // Inicjalizacja komunikacji szeregowej
-  pinMode(joystick_SW, INPUT_PULLUP);  // Przycisk z wewnętrznym pull-up
+  Serial.begin(9600);
+  pinMode(joystick_SW, INPUT_PULLUP);
+  Mouse.begin();
+  Keyboard.begin();
   
-  Serial.println("=== Joystick Arduino UNO R3 (LEWO/PRAWO ZAMIENIONE) ===");
-  Serial.println("Pozycja będzie wyświetlana TYLKO przy zmianie pozycji");
+  Serial.println("=== Joystick jako Myszka USB + Klawiatura ===");
+  Serial.println("LEWO/PRAWO: ruch myszki OK");
+  Serial.println("GÓRA/DÓŁ: ruch myszki ZAMIENIONY");
+  Serial.println("PRZYCISK JOYSTICK'A: SPACJA");
   Serial.println("");
 }
 
@@ -39,61 +49,53 @@ void loop() {
   x_value = analogRead(joystick_X);
   y_value = analogRead(joystick_Y);
   
-  // Odczyt wartości cyfrowej przycisku (0 = wciśnięty, 1 = zwolniony)
+  // Odczyt wartości cyfrowej przycisku
   button_value = digitalRead(joystick_SW);
   
   // Sprawdzenie czy X się zmienił o więcej niż próg
-  bool x_changed = (prev_x_value == -1) || (abs(x_value - prev_x_value) > sensitivity_threshold);
+  bool x_changed = abs(x_value - prev_x_value) > sensitivity_threshold;
   
   // Sprawdzenie czy Y się zmienił o więcej niż próg
-  bool y_changed = (prev_y_value == -1) || (abs(y_value - prev_y_value) > sensitivity_threshold);
+  bool y_changed = abs(y_value - prev_y_value) > sensitivity_threshold;
   
   // Sprawdzenie czy przycisk się zmienił
   bool button_changed = (prev_button_value != button_value);
   
-  // Jeśli coś się zmieniło - wyświetl
-  if (x_changed || y_changed || button_changed) {
-    Serial.print("X: ");
-    Serial.print(x_value);
-    Serial.print(" | Y: ");
-    Serial.print(y_value);
-    Serial.print(" | Przycisk: ");
-    Serial.println(button_value == 0 ? "WCISNIETY" : "ZWOLNIONY");
-    
-    // Określenie pozycji joystick'a (LEWO/PRAWO ZAMIENIONE)
-    printJoystickPosition(x_value, y_value);
-    
-    Serial.println("---");
-    
-    // Zaktualizuj poprzednie wartości
+  // Ruch myszki (X - lewo/prawo)
+  if (x_changed) {
+    int x_move = 0;
+    if (x_value < 300) {
+      x_move = -mouse_speed;  // LEWO
+    } else if (x_value > 700) {
+      x_move = mouse_speed;   // PRAWO
+    }
+    Mouse.move(x_move, 0, 0);
     prev_x_value = x_value;
+  }
+  
+  // Ruch myszki (Y - góra/dół ZAMIENIONE)
+  if (y_changed) {
+    int y_move = 0;
+    if (y_value < 300) {
+      y_move = -mouse_speed;  // GÓRA (zamienione - było dół)
+    } else if (y_value > 700) {
+      y_move = mouse_speed;   // DÓŁ (zamienione - była góra)
+    }
+    Mouse.move(0, y_move, 0);
     prev_y_value = y_value;
+  }
+  
+  // SPACJA na przycisk joystick'a
+  if (button_changed) {
+    if (button_value == 0) {
+      Keyboard.press(' ');
+      Serial.println("SPACJA: WCIŚNIĘTA");
+    } else {
+      Keyboard.release(' ');
+      Serial.println("SPACJA: ZWOLNIONA");
+    }
     prev_button_value = button_value;
   }
   
-  delay(50);  // Sprawdzaj co 50ms (nie blokuje, gdy się nic nie zmienia)
-}
-
-// Funkcja do wyświetlania kierunku joystick'a (LEWO I PRAWO ZAMIENIONE)
-void printJoystickPosition(int x, int y) {
-  Serial.print("Pozycja: ");
-  
-  // Określenie kierunku (LEWO i PRAWO ZAMIENIONE)
-  if (x < 300) {
-    Serial.print("PRAWO ");
-  } else if (x > 700) {
-    Serial.print("LEWO ");
-  } else {
-    Serial.print("SRODEK_X ");
-  }
-  
-  if (y > 700) {
-    Serial.print("| GÓRA");
-  } else if (y < 300) {
-    Serial.print("| DÓŁ");
-  } else {
-    Serial.print("| SRODEK_Y");
-  }
-  
-  Serial.println();
+  delay(20);  // Sprawdzaj co 20ms dla płynnego ruchu
 }
